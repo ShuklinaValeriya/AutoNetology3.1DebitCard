40d2ef21b1f2e57687f2f2c65fe54ba8a571183f
unknown <shuklinavaleria@yandex.ru>
2022-05-04 00:06:41 +0300

    Init commit

---
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -0,0 +1,80 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DebitCardTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp2(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testURL(){
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Марина Петрова");
+        elements.get(1).sendKeys("+79092106832");
+
+        driver.findElement(By.className("checkbox__box")).click(); // Клик по чекбоксу
+        driver.findElement(By.tagName("button")).click();// Клик по кнопке Продолжить
+
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+
+    
+    
+    
+    
+
+    @AfterEach
+    public void close(){
+        driver.quit();
+        driver = null ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
